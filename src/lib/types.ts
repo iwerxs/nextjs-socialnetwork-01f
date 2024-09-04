6640c5f36a31b90b8963ee9185ab79332d5f9053
Iw Orxs <iworxs@gmail.com>
2024-09-04 17:13:18 +0100

    post Data from Prisma

---
+++ src/lib/types.ts
@@ -0,0 +1,17 @@
+//src/lib/types.ts
+
+import { Prisma } from "@prisma/client";
+
+export const postDataInclude = {
+  user: {
+    select: {
+      username: true,
+      displayName: true,
+      avatarUrl: true,
+    },
+  },
+} satisfies Prisma.PostInclude;
+
+export type PostData = Prisma.PostGetPayload<{
+  include: typeof postDataInclude;
+}>;
