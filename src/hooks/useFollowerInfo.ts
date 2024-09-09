3024b7dea1af8ba6cf4eb12d4d0e2b79b3429e3d
Iw Orxs <iworxs@gmail.com>
2024-09-09 16:45:05 +0100

    follower info hook

---
+++ src/hooks/useFollowerInfo.ts
@@ -0,0 +1,18 @@
+//src/hooks/useFollowerInfo.ts
+//generic hook
+
+import kyInstance from "@/lib/ky";
+import { FollowerInfo } from "@/lib/types";
+import { useQuery } from "@tanstack/react-query";
+
+export default function useFollowerInfo(
+  userId: string,
+  initialState: FollowerInfo,
+) {
+  const query = useQuery({
+    queryKey: ["follower-info", userId],
+    queryFn: () =>
+      kyInstance.get(`/api/followers/${userId}`).json<FollowerInfo>(),
+  });
+  return query;
+}
