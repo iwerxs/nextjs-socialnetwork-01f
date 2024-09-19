a1f4233bd1f5d9e1e207335517cbcc1791bc13b0
Iw Orxs <iworxs@gmail.com>
2024-09-19 13:10:49 +0100

    updated TrendsSideBar err with username

--- src/components/TrendsSideBar.tsx
+++ src/components/TrendsSideBar.tsx
@@ -2,7 +2,7 @@
 
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { userDataSelect } from "@/lib/types";
+// import { userDataSelect } from "@/lib/types";
 import { Loader2 } from "lucide-react";
 import Link from "next/link";
 import { Suspense } from "react";
@@ -11,6 +11,8 @@ import { Button } from "./ui/button";
 import { unstable_cache } from "next/cache";
 import { title } from "process";
 import { formatNumber } from "@/lib/utils";
+import { getUserDataSelect } from "@/lib/types";
+import FollowButton from "./FollowButton";
 // import { setTimeout } from "timers/promises";
 
 export default function TrendsSideBar() {
@@ -39,8 +41,13 @@ async function WhoToFollow() {
       NOT: {
         id: user.id,
       },
+      followers: {
+        none: {
+          followerId: user.id,
+        },
+      },
     },
-    select: userDataSelect,
+    select: getUserDataSelect(user.id),
     take: 5,
   });
 
@@ -67,7 +74,16 @@ async function WhoToFollow() {
                 </p>
               </div>
             </Link>
-            <Button>Follow</Button>
+            {/* <Button>Follow</Button> */}
+            <FollowButton
+              userId={user.id}
+              initialState={{
+                followers: user._count.followers,
+                isFollowedByUser: user.followers.some(
+                  ({ followerId }) => followerId === user.id,
+                ),
+              }}
+            />
           </div>
         ))}
       </div>
