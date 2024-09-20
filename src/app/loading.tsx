aa1ceb6e79e8a621a27b5216a83ec8374323e099
Iw Orxs <iworxs@gmail.com>
2024-09-20 10:47:25 +0100

    general loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+//src/app/loading.tsx
+
+import { Loader2 } from "lucide-react";
+
+//loading indicator
+export default function Loading() {
+  return <Loader2 className="mx-auto my-3 animate-spin" />;
+}
