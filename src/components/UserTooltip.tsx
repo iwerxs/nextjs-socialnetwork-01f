616f7c9dd4898f6fe4460028312f13772be58c7b
Iw Orxs <iworxs@gmail.com>
2024-09-26 13:13:31 +0100

    user tooltip for user profile avatar

--- src/components/UserTooltip.tsx
+++ src/components/UserTooltip.tsx
@@ -1,5 +1,49 @@
 //src/components/UserTooltip.tsx
+"use client";
 
+import { useSession } from "@/app/(main)/SessionProvider";
+import { FollowerInfo, UserData } from "@/lib/types";
 import { PropsWithChildren } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+import Link from "next/link";
+import UserAvatar from "./UserAvatar";
 
-interface UserTooltipProps extends PropsWithChildren {}
+interface UserTooltipProps extends PropsWithChildren {
+  user: UserData;
+}
+
+//current logged in user via useSession
+export default function UserTooltip({ user, children }: UserTooltipProps) {
+  const { user: loggedInUser } = useSession();
+
+  //chk initial state of Follow-Btn
+  const followerState: FollowerInfo = {
+    followers: user._count.followers,
+    isFollowedByUser: !!user.followers.some(
+      ({ followerId }) => followerId === loggedInUser?.id,
+    ),
+  };
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent>
+          <div className="flex max-w-80 flex-col gap-3 break-words px-1 py-2.5 md:min-w-52">
+            <div className="flex items-center justify-between gap-2">
+              {/* UserAvatar from Next Link */}
+              <Link href={`/users/${user.username}`}>
+                <UserAvatar size={70} avatarUrl={user.avatarUrl} />
+              </Link>
+            </div>
+          </div>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
